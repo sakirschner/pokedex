4b7da9bda48df51c3506b1564a8d1472e886a7ec
scott <scottakirschner@gmail.com>
2020-05-07 22:22:57 -0400

    styling

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Info = () => {
-    return (
-        <div>
-            <p>Developed By Scott Kirschner</p>
-        </div>
-    )
-}
+  return (
+    <div className="info">
+      <a href="https://www.scottkirschner.com" class="nes-badge is-icon">
+        <span class="is-dark">hi</span>
+        <span class="is-warning name">Scott Kirschner</span>
+      </a>
+    </div>
+  );
+};
 
 export default Info;
